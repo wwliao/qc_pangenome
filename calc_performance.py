7fb0c786ea43d4bba00e4bc8e590fe353ea09a56
Wen-Wei Liao <wen-wei.liao@wustl.edu>
2021-08-31 14:43:00 -0400

    Update performance calculation

--- calc_performance.py
+++ calc_performance.py
@@ -20,34 +20,28 @@ with open(args.query) as infile:
         total_query += 1
 
 truth_tp = 0
-truth_fn = 0
 with open(args.truthcov) as infile:
     for line in infile:
         cols = line.strip().split("\t")
         cov = float(cols[5])
         if cov >= args.coverage:
             truth_tp += 1
-        else:
-            truth_fn += 1
 
-truth_fn = total_truth - (truth_tp + truth_fn) + truth_fn
+truth_fn = total_truth - truth_tp
 
 query_tp = 0
-query_fp = 0
 with open(args.querycov) as infile:
     for line in infile:
         cols = line.strip().split("\t")
         cov = float(cols[5])
         if cov >= args.coverage:
             query_tp += 1
-        else:
-            query_fp += 1
 
-query_fp = total_query - (query_tp + query_fp) + query_fp
+query_fp = total_query - query_tp
 
 recall = truth_tp / (truth_tp + truth_fn)*100
 precision = query_tp / (query_tp + query_fp)*100
 f1 = 2*recall*precision / (recall + precision)
-print(f"Recall: {recall:.2f}%")
-print(f"Precision: {precision:.2f}%")
+print(f"Recall: {truth_tp}/({truth_tp} + {truth_fn}) = {recall:.2f}%")
+print(f"Precision: {query_tp}/({query_tp} + {query_fp}) = {precision:.2f}%")
 print(f"F1: {f1:.2f}%")
